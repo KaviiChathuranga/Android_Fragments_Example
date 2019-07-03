66ef1d23011c86529e747ce4b9aa0c6b04e245b8
kavindu <chathurangakavindu67@gmail.com>
2019-07-03 18:11:37 +0530

    fragment commit 2019/07/03

--- app/src/main/java/com/example/fragments/SearchFragment.java
+++ app/src/main/java/com/example/fragments/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,5 +9,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SearchFragment extends Fragment {
+    Activity context;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        context = getActivity();
+        return inflater.inflate(R.layout.fragment_search,container, false);
+    }
+
+    public void onStart(){
+        super.onStart();
+        Initializations();
+        Action();
+        hideOrClearNavBar();
+    }
+
+    private void Action() {
+    }
+
+    private void Initializations() {
+    }
+
+    private void hideOrClearNavBar() {
+        //        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getActivity().getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 }
