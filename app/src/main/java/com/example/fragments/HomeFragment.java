675fe0ecb92c25a928e3247b2cf224a9d8d1013f
kavindu <chathurangakavindu67@gmail.com>
2019-07-10 16:12:21 +0530

    Fragments with Facebook Authentication

--- app/src/main/java/com/example/fragments/HomeFragment.java
+++ app/src/main/java/com/example/fragments/HomeFragment.java
@@ -1,21 +1,132 @@
 package com.example.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+
+import static com.facebook.AccessTokenManager.TAG;
 
 public class HomeFragment extends Fragment {
+
+    private CallbackManager mCallbackManager;
+    Activity context;
+    private FirebaseAuth mAuth;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         hideOrClearNavBar();
+        context = getActivity();
+
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
+
+    @Override
+    public void onStart() {
+//        FirebaseApp.initializeApp(this.getContext());
+        super.onStart();
+//        Initializations();
+    }
+
+//    private void Initializations() {
+//
+//        mAuth = FirebaseAuth.getInstance();
+//        mCallbackManager = CallbackManager.Factory.create();
+//        LoginButton loginButton = context.findViewById(R.id.login_button);
+//        loginButton.setReadPermissions("email", "public_profile");
+//        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+//                handleFacebookAccessToken(loginResult.getAccessToken());
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//                Log.d(TAG, "facebook:onCancel");
+//                // ...
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//                Log.d(TAG, "facebook:onError", error);
+//                // ...
+//            }
+//        });
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser!=null){
+//            updateUI(currentUser);
+//        }
+//
+//
+//    }
+
+//    private void updateUI(FirebaseUser currentUser) {
+//        System.out.println("USER LOGGED");
+//        Intent intent = new Intent(context,LoggedActivity.class);
+//        startActivity(intent);
+//
+//    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        // Pass the activity result back to the Facebook SDK
+//        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+//    }
+
+//    private void handleFacebookAccessToken(AccessToken token) {
+//        Log.d(TAG, "handleFacebookAccessToken:" + token);
+//
+//        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+//        mAuth.signInWithCredential(credential)
+//                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithCredential:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+//                            System.out.println("Authentication failed.");
+//                            updateUI(null);
+//                        }
+//
+//                    }
+//                });
+//    }
 
     private void hideOrClearNavBar() {
         //        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fragments;
 
+import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
@@ -13,21 +15,49 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements
-        NavigationView.OnNavigationItemSelectedListener{
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+import static com.facebook.AccessTokenManager.TAG;
+
+public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
+    private FirebaseAuth mAuth;
+    private CallbackManager mCallbackManager;
+    Activity context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("onCreate");
         onWindowFocusChanged(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        context = this;
+        final FirebaseApp firebaseApp = FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,17 +70,93 @@ public class MainActivity extends AppCompatActivity implements
         NavigationView drawerView = findViewById(R.id.drawer_navigation);
 
         navigationView.setOnNavigationItemSelectedListener(listener);
-        drawerView.setNavigationItemSelectedListener(this);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+        Initializations();
+
+    }
+    private void Initializations() {
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = context.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(loginClick);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
+
 
 
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            updateUI(currentUser);
+        }
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        System.out.println("USER LOGGED");
+        Intent intent = new Intent(context,LoggedActivity.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result back to the Facebook SDK
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        System.out.println("handleFacebookAccessToken");
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            System.out.println("Authentication failed.");
+                            updateUI(null);
+                        }
+
+                    }
+                });
+    }
+    @Override
     public void onBackPressed() {
+
         if(drawerLayout.isDrawerOpen(GravityCompat.START)){
             drawerLayout.closeDrawer(GravityCompat.START);
+            System.out.println("isDrawerOpen");
         }else {
             super.onBackPressed();
         }
@@ -60,7 +166,7 @@ public class MainActivity extends AppCompatActivity implements
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             Fragment fragment = null;
-
+            System.out.println("OnNavigationItemSelectedListener");
             switch (menuItem.getItemId()){
                 case R.id.nav_home:
                     fragment = new HomeFragment();
@@ -81,6 +187,7 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
+        System.out.println("onWindowFocusChanged");
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
             getWindow().getDecorView().setSystemUiVisibility(
@@ -92,28 +199,13 @@ public class MainActivity extends AppCompatActivity implements
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
+    View.OnClickListener loginClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            LoginManager.getInstance().logInWithReadPermissions(context, Arrays.asList("public_profile"));
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Fragment fragment = null;
-
-        switch (menuItem.getItemId()){
-            case R.id.drawer_home:
-                fragment = new HomeFragment();
-                break;
-
-            case R.id.drawer_notification:
-                fragment = new NotificationFragment();
-                break;
-
-            case R.id.drawer_new:
-                fragment = new NewFragment();
-                break;
         }
+    };
 
-        drawerLayout.closeDrawer(GravityCompat.START);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
-        return true;
-    }
+
 }
